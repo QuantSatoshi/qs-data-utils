57af727053002d6e4f2f8ed97b24e8e0738c8e3f
wanghao <whateverhow@gmail.com>
2023-03-16 21:27:20 -0500

    [master]   (use "git restore --staged <file>..." to unstage)

--- dist/utils/dataPathUtils.js
+++ dist/utils/dataPathUtils.js
@@ -4,7 +4,7 @@ exports.getDataFileName = void 0;
 const qs_js_utils_1 = require("qs-js-utils");
 function getDataFileName(channel, exchange, pairDb, startDate) {
     const utcDate = (0, qs_js_utils_1.getDay)(startDate);
-    const outputFileName = `${channel}/${exchange}/${channel}-${exchange}-${pairDb}-${utcDate}.gz`;
+    const outputFileName = `${channel}/${exchange}/${pairDb}/${channel}-${exchange}-${pairDb}-${utcDate}.gz`;
     return outputFileName;
 }
 exports.getDataFileName = getDataFileName;
--- src/utils/dataPathUtils.ts
+++ src/utils/dataPathUtils.ts
@@ -2,6 +2,6 @@ import { getDay } from 'qs-js-utils';
 
 export function getDataFileName(channel: string, exchange: string, pairDb: string, startDate: string | Date | number) {
   const utcDate = getDay(startDate);
-  const outputFileName = `${channel}/${exchange}/${channel}-${exchange}-${pairDb}-${utcDate}.gz`;
+  const outputFileName = `${channel}/${exchange}/${pairDb}/${channel}-${exchange}-${pairDb}-${utcDate}.gz`;
   return outputFileName;
 }
